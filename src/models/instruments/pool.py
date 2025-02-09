fc9fd4b163f9ef9dd56fef543199c06e0e13507b
api_massta <blackmassta@pm.me>
2025-02-09 00:54:59 -0500

    Make values optional

--- src/models/instruments/pool.py
+++ src/models/instruments/pool.py
@@ -9,77 +9,77 @@ from src.utils.scripts import convert_percentage
 
 
 class TokenReserves(ApiModel):
-    reserves: Optional[float]
-    reserves_in_usd: Optional[float]
+    reserves: Optional[float] = None
+    reserves_in_usd: Optional[float] = None
 
 
 class TokenValueData(ApiModel):
-    fdv_in_usd: Optional[float]
-    market_cap_in_usd: Optional[float]
-    market_cap_to_holders_ratio: Optional[float]
+    fdv_in_usd: Optional[float] = None
+    market_cap_in_usd: Optional[float] = None
+    market_cap_to_holders_ratio: Optional[float] = None
 
 
 class HistoricalDataEntry(ApiModel):
-    swaps_count: Optional[int]
-    buyers_count: Optional[int]
-    price_in_usd: Optional[float]
-    sellers_count: Optional[int]
-    volume_in_usd: Optional[float]
-    buy_swaps_count: Optional[int]
-    sell_swaps_count: Optional[int]
+    swaps_count: Optional[int] = None
+    buyers_count: Optional[int] = None
+    price_in_usd: Optional[float] = None
+    sellers_count: Optional[int] = None
+    volume_in_usd: Optional[float] = None
+    buy_swaps_count: Optional[int] = None
+    sell_swaps_count: Optional[int] = None
 
 
 class SentimentVotes(ApiModel):
-    total: Optional[int]
-    up_percentage: Optional[float]
-    down_percentage: Optional[float]
+    total: Optional[int] = None
+    up_percentage: Optional[float] = None
+    down_percentage: Optional[float] = None
 
 
 class GTScoreDetails(ApiModel):
-    info: Optional[float]
-    pool: Optional[float]
-    transactions: Optional[float]
-    holders: Optional[float]
-    creation: Optional[float]
+    info: Optional[float] = None
+    pool: Optional[float] = None
+    transactions: Optional[float] = None
+    holders: Optional[float] = None
+    creation: Optional[float] = None
 
 
 class HighLowPriceData(ApiModel):
-    high_price_in_usd_24h: Optional[float]
-    high_price_timestamp_24h: Optional[datetime]
-    low_price_in_usd_24h: Optional[float]
-    low_price_timestamp_24h: Optional[datetime]
+    high_price_in_usd_24h: Optional[float] = None
+    high_price_timestamp_24h: Optional[datetime] = None
+    low_price_in_usd_24h: Optional[float] = None
+    low_price_timestamp_24h: Optional[datetime] = None
 
 
 class Attributes(ApiModel):
-    address: Optional[str]
-    name: Optional[str]
-    fully_diluted_valuation: Optional[float]
-    base_token_id: Optional[str]
-    price_in_usd: Optional[float]
-    price_in_target_token: Optional[float]
-    reserve_in_usd: Optional[float]
-    reserve_threshold_met: Optional[bool]
-    from_volume_in_usd: Optional[float]
-    to_volume_in_usd: Optional[float]
-    api_address: Optional[str]
+    address: Optional[str] = None
+    name: Optional[str] = None
+    fully_diluted_valuation: Optional[float] = None
+    base_token_id: Optional[str] = None
+    price_in_usd: Optional[float] = None
+    price_in_target_token: Optional[float] = None
+    reserve_in_usd: Optional[float] = None
+    reserve_threshold_met: Optional[bool] = None
+    from_volume_in_usd: Optional[float] = None
+    to_volume_in_usd: Optional[float] = None
+    api_address: Optional[str] = None
     pool_fee: Optional[float] = None
     token_weightages: Optional[dict] = None
     token_reserves: Dict[str, TokenReserves]
     token_value_data: Dict[str, TokenValueData]
     balancer_pool_id: Optional[str] = None
-    swap_count_24h: Optional[int]
-    swap_url: Optional[str]
-    sentiment_votes: SentimentVotes
-    price_percent_change: Optional[str]
-    price_percent_changes: Dict[str, str]
+    swap_count_24h: Optional[int] = None
+    swap_url: Optional[str] = None
+    sentiment_votes: Optional[SentimentVotes] = None
+    price_percent_change: Optional[str] = None
+    price_percent_changes: Dict[str, str] = None
     historical_data: Dict[str, HistoricalDataEntry]
     locked_liquidity: Optional[float] = None
     security_indicators: List = []
     gt_score: Optional[float] = None
     gt_score_details: Optional[GTScoreDetails] = None
-    pool_reports_count: Optional[int]
-    pool_created_at: datetime
-    latest_swap_timestamp: datetime
+    pool_reports_count: Optional[int] = None
+    pool_created_at: Optional[datetime] = None
+    latest_swap_timestamp: Optional[datetime] = None
     high_low_price_data_by_token_id: Dict[str, HighLowPriceData]
     is_nsfw: Optional[bool] = None
     is_stale_pool: Optional[bool] = None
--- src/models/instruments/transaction.py
+++ src/models/instruments/transaction.py
@@ -1,40 +1,42 @@
+from typing import Optional
+
 from src.models.integration.api import ApiModel
 
 
 class TransactionEntry(ApiModel):
-    buys: int
-    sells: int
+    buys: Optional[int] = None
+    sells: Optional[int] = None
 
 
 class TransactionData(ApiModel):
-    last_300_s: TransactionEntry
-    last_900_s: TransactionEntry
-    last_1800_s: TransactionEntry
-    last_3600_s: TransactionEntry
-    last_7200_s: TransactionEntry
-    last_21600_s: TransactionEntry
-    last_43200_s: TransactionEntry
-    last_86400_s: TransactionEntry
-    last_172800_s: TransactionEntry
+    last_300_s: Optional[TransactionEntry] = None
+    last_900_s: Optional[TransactionEntry] = None
+    last_1800_s: Optional[TransactionEntry] = None
+    last_3600_s: Optional[TransactionEntry] = None
+    last_7200_s: Optional[TransactionEntry] = None
+    last_21600_s: Optional[TransactionEntry] = None
+    last_43200_s: Optional[TransactionEntry] = None
+    last_86400_s: Optional[TransactionEntry] = None
+    last_172800_s: Optional[TransactionEntry] = None
 
 
 class Attributes(ApiModel):
-    base_symbol: str
-    base_address: str
-    base_price_in_currency: float
-    base_price_in_usd_percent_change: float
-    quote_symbol: str
-    quote_address: str
-    quote_price_in_currency: float
-    quote_price_in_usd_percent_change: float
-    base_price_in_quote: float
-    quote_price_in_base: float
-    volume_in_currency: float
-    volume_in_usd: float
-    transaction_data: TransactionData
+    base_symbol: Optional[str] = None
+    base_address: Optional[str] = None
+    base_price_in_currency: Optional[float] = None
+    base_price_in_usd_percent_change: Optional[float] = None
+    quote_symbol: Optional[str] = None
+    quote_address: Optional[str] = None
+    quote_price_in_currency: Optional[float] = None
+    quote_price_in_usd_percent_change: Optional[float] = None
+    base_price_in_quote: Optional[float] = None
+    quote_price_in_base: Optional[float] = None
+    volume_in_currency: Optional[float] = None
+    volume_in_usd: Optional[float] = None
+    transaction_data: Optional[TransactionData] = None
 
 
 class PoolTransactionData(ApiModel):
     id: str
     type: str
-    attributes: Attributes
+    attributes: Optional[Attributes] = None
